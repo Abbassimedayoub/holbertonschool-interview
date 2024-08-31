b606da2b8ed05ddb763b5a6d6a884dd7171e4da7
abbassimedayoub <abbassimedayoub@gamil.com>
2024-08-31 18:04:13 +0200

    pep8 validation

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -5,8 +5,10 @@
 def canUnlockAll(boxes):
     """
     Return if all boxex can be unlocked
-    :parm boxes : list of list wehere each list represent the compoment of a box
-    :return: True if all boxes can be opened and false otherwise
+    :parm boxes : list of list wehere each list 
+    represent the compoment of a box
+    :return: True if all boxes can be opened
+    and false otherwise
     """
     n = len(boxes)
     unloked = [False] * n  # initialise that all boxes are loked
